5bbdd11d1c81cc4233786ac9f08e0854f3ea9284
Ankush Thakur <ankushthakur5981@gmail.com>
2026-06-20 13:36:58 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 const connectToMongoDB = require("./config/db");
