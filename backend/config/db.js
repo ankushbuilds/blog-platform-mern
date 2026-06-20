6175abe59aa145665986aa22e761704092b07760
Ankush Thakur <ankushthakur5981@gmail.com>
2026-06-20 13:34:13 +0530

    minor change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log("✅ Connected to MongoDB successfully");
   } catch (err) {
-    console.error("❌ MongoDB connection error:");
-    console.error(err);
-
-    // Exit so the app doesn't run without DB
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
